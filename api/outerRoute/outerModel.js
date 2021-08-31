933d115b27e20b9cf2d035e3081b420283cd2d61
rajvardhan1 <rajvardhan@webkorps.com>
2021-08-31 18:01:16 +0530

    Invoice mail and message integrated

--- api/outerRoute/outerModel.js
+++ api/outerRoute/outerModel.js
@@ -1,19 +1,28 @@
 const { captureCardCharge } = require('../stripe');
 const ejs = require('ejs');
+const { sendSMS } = require('../../twilio.config');
+const { transporter, mailOptions } = require('../../nodemailer.config');
 
 const stripeCardChargeModel = async (req, res) => {
-    // const { email, subject, mailBody } = req.body;
-    const result = await captureCardCharge(req, res);
+    const { email, phoneNumber, firstName, lastName, addressLine1, postalCode, city,
+        state, country, products, total } = req.body;
+    // const result = await captureCardCharge(req, res);
 
-    return result;
-    // const body = await ejs.renderFile(ROOT_DIR + '/templates/invoice.ejs');
-
-    // if (result) {
-    //     const response = await transporter.sendMail(mailOptions(email, subject, body), (error) => {
-    //         console.log(error, ' mailError')
-    //         res.send(error)
-    //     })
-    // }
+    if (false) {
+    } else {
+        const body = await ejs.renderFile(ROOT_DIR + '/templates/invoice.ejs', {
+            firstName, lastName, addressLine1, postalCode, city,
+            state, country, products, total
+        });
+        const subject = "Invoice mail"
+        const response = await transporter.sendMail(mailOptions(email, subject, body), (error) => {
+            console.log(error, ' mailError')
+            res.send(error)
+        })
+        const message = "You have successfully purchased the product"
+        await sendSMS(message, phoneNumber)
+    }
+    // return result;
 }
 
 module.exports = {
